1d22dee5d56e5b74a4db2ec5ef4e5b6da2747e26
adam <kisferencadam@gmail.com>
2015-08-27 23:52:05 +0200

    working mock of pie chart

--- app/src/main/java/com/adamkis/astronchallenge/ui/MainActivity.java
+++ app/src/main/java/com/adamkis/astronchallenge/ui/MainActivity.java
@@ -1,6 +1,8 @@
 package com.adamkis.astronchallenge.ui;
 
 import android.app.Dialog;
+import android.content.Intent;
+import android.graphics.Color;
 import android.support.v7.app.ActionBarActivity;
 import android.support.v4.app.Fragment;
 import android.os.Bundle;
@@ -12,6 +14,7 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.LinearLayout;
 
 import com.adamkis.astronchallenge.R;
 import com.adamkis.astronchallenge.common.Const;
@@ -22,6 +25,12 @@ import com.adamkis.astronchallenge.network.VolleySingleton;
 import com.android.volley.Response;
 import com.android.volley.VolleyError;
 import com.android.volley.toolbox.NetworkImageView;
+
+import org.achartengine.ChartFactory;
+import org.achartengine.model.CategorySeries;
+import org.achartengine.model.XYSeries;
+import org.achartengine.renderer.DefaultRenderer;
+import org.achartengine.renderer.SimpleSeriesRenderer;
 
 import java.util.ArrayList;
 import java.util.Arrays;
@@ -103,9 +112,72 @@ public class MainActivity extends ActionBarActivity {
 
             download();
 
+
+
+            LinearLayout chartContainer = (LinearLayout) rootView.findViewById(R.id.chart);
+            openChart( chartContainer );
+
+
             return rootView;
         }
 
+        private void openChart(ViewGroup chartContainer) {
+
+            // Pie Chart Section Names
+            String[] code = new String[] { "Froyo", "Gingerbread",
+                    "IceCream Sandwich", "Jelly Bean", "KitKat" };
+
+            // Pie Chart Section Value
+            double[] distribution = { 0.5, 9.1, 7.8, 45.5, 33.9 };
+
+            // Color of each Pie Chart Sections
+            int[] colors = { Color.BLUE, Color.MAGENTA, Color.GREEN, Color.CYAN,
+                    Color.RED };
+
+            // Instantiating CategorySeries to plot Pie Chart
+            CategorySeries distributionSeries = new CategorySeries(
+                    " Android version distribution as on October 1, 2012");
+            for (int i = 0; i < distribution.length; i++) {
+                // Adding a slice with its values and name to the Pie Chart
+                distributionSeries.add(code[i], distribution[i]);
+            }
+
+            // Instantiating a renderer for the Pie Chart
+            DefaultRenderer defaultRenderer = new DefaultRenderer();
+            for (int i = 0; i < distribution.length; i++) {
+                SimpleSeriesRenderer seriesRenderer = new SimpleSeriesRenderer();
+                seriesRenderer.setColor(colors[i]);
+                seriesRenderer.setDisplayChartValues(true);
+//Adding colors to the chart
+                defaultRenderer.setBackgroundColor(Color.BLACK);
+                defaultRenderer.setApplyBackgroundColor(true);
+                // Adding a renderer for a slice
+                defaultRenderer.addSeriesRenderer(seriesRenderer);
+            }
+
+            defaultRenderer
+                    .setChartTitle("Android version distribution as on December 1, 2014. ");
+            defaultRenderer.setChartTitleTextSize(20);
+            defaultRenderer.setZoomButtonsVisible(false);
+
+            // this part is used to display graph on the xml
+            // Creating an intent to plot bar chart using dataset and
+            // multipleRenderer
+            // Intent intent = ChartFactory.getPieChartIntent(getBaseContext(),
+            // distributionSeries , defaultRenderer, "AChartEnginePieChartDemo");
+
+            // Start Activity
+            // startActivity(intent);
+
+            // remove any views before u paint the chart
+            chartContainer.removeAllViews();
+            // drawing pie chart
+            View mChart = ChartFactory.getPieChartView(getActivity(),
+                    distributionSeries, defaultRenderer);
+            // adding the view to the linearlayout
+            chartContainer.addView(mChart);
+
+        }
 
         @Override
         public void onPause() {
