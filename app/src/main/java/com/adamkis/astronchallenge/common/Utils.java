9cd865b461b7fc2cf500a729ef3f4c1ee98240c0
adam <kisferencadam@gmail.com>
2015-08-28 20:52:45 +0200

    better loading animation and fade

---
+++ app/src/main/java/com/adamkis/astronchallenge/common/Utils.java
@@ -0,0 +1,65 @@
+package com.adamkis.astronchallenge.common;
+
+import android.view.View;
+
+/**
+ * Created by adam on 8/28/15.
+ */
+public class Utils {
+
+
+    public static void crossfadeViews(View firstView, View secondView, boolean showFirst, boolean animate, int animationDurationMillis){
+
+        float loadingAlpha;
+        float contentAlpha;
+
+        if( showFirst ){
+            loadingAlpha = 1f;
+            contentAlpha = 0f;
+        }
+        else{
+            loadingAlpha = 0f;
+            contentAlpha = 1f;
+        }
+
+        if( firstView != null && firstView.getAlpha() != loadingAlpha ){
+            if( animate ) {
+                firstView.animate()
+                        .alpha(loadingAlpha)
+                        .setDuration(animationDurationMillis)
+//                        .setInterpolator(new DecelerateInterpolator())
+                        .setListener(null);
+            }
+            else{
+                firstView.setAlpha(loadingAlpha);
+            }
+        }
+        if( secondView != null && secondView.getAlpha() != contentAlpha ) {
+            if( animate ) {
+                secondView.animate()
+                        .alpha(contentAlpha)
+                        .setDuration(animationDurationMillis)
+//                        .setInterpolator(new AccelerateDecelerateInterpolator())
+                        .setListener(null);
+            }
+            else{
+                secondView.setAlpha(contentAlpha);
+            }
+        }
+
+    }
+
+    private static final int loadingAnimationDurationMillis = 3000;
+
+    public static void hideLoadingAnimated( View loading, View container ){
+        crossfadeViews(loading, container, false, true, loadingAnimationDurationMillis);
+    }
+
+
+    public static void revealViewAnimated( View view ){
+        crossfadeViews(view, null, true, true, loadingAnimationDurationMillis);
+    }
+
+
+
+}
--- app/src/main/java/com/adamkis/astronchallenge/ui/MainActivity.java
+++ app/src/main/java/com/adamkis/astronchallenge/ui/MainActivity.java
@@ -1,54 +1,36 @@
 package com.adamkis.astronchallenge.ui;
 
 import android.app.Activity;
-import android.app.Dialog;
 import android.content.Intent;
-import android.graphics.Color;
-import android.support.v4.app.ActivityCompat;
-import android.support.v7.app.ActionBarActivity;
-import android.support.v4.app.Fragment;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.util.Log;
-import android.view.LayoutInflater;
-import android.view.Menu;
-import android.view.MenuItem;
 import android.view.View;
-import android.view.ViewGroup;
 import android.widget.Button;
-import android.widget.LinearLayout;
 
 import com.adamkis.astronchallenge.R;
 import com.adamkis.astronchallenge.common.Const;
+import com.adamkis.astronchallenge.common.Utils;
 import com.adamkis.astronchallenge.model.Attendee;
-import com.adamkis.astronchallenge.model.Name;
 import com.adamkis.astronchallenge.network.GsonRequest;
 import com.adamkis.astronchallenge.network.VolleySingleton;
 import com.android.volley.Response;
 import com.android.volley.VolleyError;
-import com.android.volley.toolbox.NetworkImageView;
-
-import org.achartengine.ChartFactory;
-import org.achartengine.model.CategorySeries;
-import org.achartengine.model.XYSeries;
-import org.achartengine.renderer.DefaultRenderer;
-import org.achartengine.renderer.SimpleSeriesRenderer;
 
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collections;
-import java.util.List;
 
 
 public class MainActivity extends Activity {
 
 
-    private RecyclerView mRecyclerView;
+    private RecyclerView searchResultContainer;
     private LinearLayoutManager mLayoutManager;
     private SearchResultAdapter mAdapter;
-    private Dialog loadingDialog;
-    private Button btn_go_to_chart;
+    private Button btnGoToChart;
+    private View loadingProgressBar;
     private ArrayList<Attendee> attendees;
 
 
@@ -57,31 +39,20 @@ public class MainActivity extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        searchResultContainer = (RecyclerView) findViewById(R.id.searchResultContainer);
 
+        btnGoToChart = (Button) findViewById(R.id.btnGoToChart);
+        loadingProgressBar =  findViewById(R.id.loadingProgressBar);
 
-        mRecyclerView = (RecyclerView) findViewById(R.id.my_recycler_view);
+        searchResultContainer.setHasFixedSize(true);
 
-        btn_go_to_chart = (Button) findViewById(R.id.btn_go_to_chart);
-
-        // use this setting to improve performance if you know that changes
-        // in content do not change the layout size of the RecyclerView
-        mRecyclerView.setHasFixedSize(true);
-
-        // use a linear layout manager
         mLayoutManager = new LinearLayoutManager(this);
-        mRecyclerView.setLayoutManager(mLayoutManager);
-
-
-        loadingDialog = new Dialog(this ,android.R.style.Theme_Translucent_NoTitleBar);
-//            loadingDialog = new Dialog(getActivity());
-//            loadingDialog.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
-        loadingDialog.setContentView(R.layout.dialog_loading);
-        loadingDialog.show();
+        searchResultContainer.setLayoutManager(mLayoutManager);
 
         download();
 
 
-        btn_go_to_chart.setOnClickListener(new View.OnClickListener() {
+        btnGoToChart.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Intent intent = new Intent(MainActivity.this, ChartActivity.class);
@@ -118,11 +89,13 @@ public class MainActivity extends Activity {
 
                         Collections.sort(attendees);
 
-                        // specify an adapter (see also next example)
-                        mAdapter = new SearchResultAdapter(attendees);
-                        mRecyclerView.setAdapter(mAdapter);
 
-                        loadingDialog.dismiss();
+                        mAdapter = new SearchResultAdapter(attendees);
+                        searchResultContainer.setAdapter(mAdapter);
+
+                        Utils.hideLoadingAnimated(loadingProgressBar, searchResultContainer);
+                        Utils.revealViewAnimated(btnGoToChart);
+
 
                     }
                 }, new Response.ErrorListener() {
